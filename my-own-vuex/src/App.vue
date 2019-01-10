cf19f357217c7ea69dc93bface0cafa44c3f1ba5
jackieyin <jackieyin@tencent.com>
2019-01-10 10:28:47 +0800

    2.完成最简单的通过vuex定义全局变量，在任何一个页面可以通过

--- my-own-vuex/src/App.vue
+++ my-own-vuex/src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    干净项目
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  created() {
+    console.log('打印出this.$store.state.count的结果',this.$store.state.count);
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- my-own-vuex/src/main.js
+++ my-own-vuex/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from './myVuex/index'
 
-Vue.config.productionTip = false
+
+let store = new Vuex.Store({
+  state: {
+    count: 0
+  }
+}, Vue);
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
+
+
+
+Vue.config.productionTip = false;
